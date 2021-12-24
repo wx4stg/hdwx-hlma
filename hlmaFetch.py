a4402d564e120b5bceca3dbcbf70cb6905c7f9f5
Sam Gardner <stgardner4@tamu.edu>
2021-12-24 14:42:50 -0600

    add empty list in case first run

--- hlmaFetch.py
+++ hlmaFetch.py
@@ -3,7 +3,7 @@
 # Created 23 December 2021 by Sam Gardner <stgardner4@tamu.edu>
 
 from datetime import datetime as dt, timedelta
-from os import path, listdir, terminal_size
+from os import path, listdir, sep
 import json
 from shutil import copyfile
 from pathlib import Path
@@ -31,20 +31,22 @@ if __name__ == '__main__':
     currentHourInt = int(dt.strftime(now, "%Y%m%d%H00"))
     currentTimeInt = int(dt.strftime(now, "%Y%m%d%H%M"))
     filesToCopy = list()
-    lmaDataBasePath = path.join("home", "lma_admin", "lma", "realtime", "processed_data")
+    lmaDataBasePath = path.join(path.abspath(sep), "home", "lma_admin", "lma", "realtime", "processed_data")
     dataInputPath = path.join(basePath, "lightningin")
     Path(dataInputPath).mkdir(parents=True, exist_ok=True)
     for min in range(0, 60):
         if (lastHourInt + min) not in generatedFrames:
-            hypPathLastHour = path.join(lmaDataBasePath, dt.strftime(oneHourAgo, "%Y")[-2:]+dt.strftime(oneHourAgo, "%m%d"), dt.strftime(oneHourAgo ,"%H"), dt.strftime(oneHourAgo, "%m"))
-            contentsOfLastHour = listdir(hypPathLastHour)
-            for file in contentsOfLastHour:
-                if file.endswith(".dat.gz"):
-                    copyfile(path.join(hypPathLastHour, file), dataInputPath)
+            hypPathLastHour = path.join(lmaDataBasePath, dt.strftime(oneHourAgo, "%Y")[-2:]+dt.strftime(oneHourAgo, "%m%d"), dt.strftime(oneHourAgo ,"%H"), str(min))
+            if path.exists(hypPathLastHour):
+                contentsOfLastHour = listdir(hypPathLastHour)
+                for file in contentsOfLastHour:
+                    if file.endswith(".dat.gz"):
+                        copyfile(path.join(hypPathLastHour, file), path.join(dataInputPath, file))
         if (currentHourInt + min) not in generatedFrames:
             if (currentHourInt + min) < currentTimeInt:
-                hypPathCurrentHour = path.join(lmaDataBasePath, dt.strftime(now, "%Y")[-2:]+dt.strftime(now, "%m%d"), dt.strftime(now ,"%H"), dt.strftime(now, "%m"))
-                contentsOfCurrentHour = listdir(hypPathCurrentHour)
-                for file in contentsOfCurrentHour:
-                    if file.endswith(".dat.gz"):
-                        copyfile(path.join(hypPathCurrentHour, file), dataInputPath)
+                hypPathCurrentHour = path.join(lmaDataBasePath, dt.strftime(now, "%Y")[-2:]+dt.strftime(now, "%m%d"), dt.strftime(now ,"%H"), str(min))
+                if path.exists(hypPathCurrentHour):
+                    contentsOfCurrentHour = listdir(hypPathCurrentHour)
+                    for file in contentsOfCurrentHour:
+                        if file.endswith(".dat.gz"):
+                            copyfile(path.join(hypPathCurrentHour, file), path.join(dataInputPath, file))
