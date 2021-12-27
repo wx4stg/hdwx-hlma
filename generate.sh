4e29a6e28ea73074d1e1b7e5c4821dfb958909ec
Sam Gardner <stgardner4@tamu.edu>
2021-12-27 00:33:04 -0600

    add dummy generate.sh

---
+++ generate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "I dont want this to be operationally run yet but I need to move this to hdwx-operational to test the data fetching. So I'm putting this dummy script here so as not to crash all.sh"
